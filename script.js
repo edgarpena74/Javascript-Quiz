91c242642619cdd7ae800aca1407a122fb88d1fb
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-18 20:01:41 -0700

    Intro page linked to quiz page. JS in progress.

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
 //i might need to add prevent default behavior.
-let omg = document.getElementsByName(omg)
-function quizHTML (){
-    document.querySelector("#quiz").innerHTML = " <div id="quiz">
-    <section id="centerBox" class="flex-box">
-        <div id = "container" class="container">
-            <div id="jsHeader" class="row col-md-12"><h2>Javascript Quiz</h2></div>
-            <div id="questionSection" class="row col-md-12"><span id="question">
-                questions will go here questions will go here 
-                questions will go here  questions will go here 
-                questions will go here  questions will go here 
-            </span></div>
-            <br>
-            <div id="answerSection">
-                <div class="row">
-                    <div class="col-md-12">
-                        <button type="submit"> A </button>
-                        <span id="answerA">ANSWER WILL GO HERE</span>
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-md-12">
-                        <button type="submit"> B </button>
-                        <span id="answerB">ANSWER WILL GO HERE</span>
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-md-12">
-                        <button type="submit"> C </button>
-                        <span id="answerC">ANSWER WILL GO HERE</span>
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-md-12">
-                        <button type="submit"> D </button>
-                        <span id="answerD">ANSWER WILL GO HERE</span>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-
-    </section>
-</div>  ";
-
-}
-
-console.log (omg)
+let question = document.querySelector("#question");
+let answerSection = document.querySelector("#answerSection");
+let submitA = document.querySelector("#submitA");
+let answerA = document.querySelector("#answerA");
+let submitB = document.querySelector("#submitB");
+let answerB = document.querySelector("#answerB");
+let submitC = document.querySelector("#submitC");
+let answerC = document.querySelector("#answerC");
+let submitD = document.querySelector("#submitD");
+let answerD = document.querySelector("#answerD");
