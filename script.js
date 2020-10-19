931bcb015829ec9f5dbc28ea8acc300697d4e81a
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-19 14:30:49 -0700

    made some basic functions which i will potentially use

--- script.js
+++ script.js
@@ -9,3 +9,48 @@ let submitC = document.querySelector("#submitC");
 let answerC = document.querySelector("#answerC");
 let submitD = document.querySelector("#submitD");
 let answerD = document.querySelector("#answerD");
+let next = document.querySelector("#next");
+
+let begin = confirm("Click OK to begin the quiz")
+console.log(begin)
+
+
+
+function nu1 () {
+    question.textContent = "do you want the funk?";
+    answerA.textContent = "yeah";
+    answerB.textContent = "funky";
+    answerC.textContent = " funky funk";
+    answerD.textContent = "we want the funk!";
+}
+console.log(nu1)
+
+function nu2 () {
+    question.textContent = "do you want the funk?";
+    answerA.textContent = " yeah";
+    answerB.textContent = "funky";
+    answerC.textContent = " funky funk";
+    answerD.textContent = "we want the funk!";
+}
+console.log(nu2)
+
+function nu3 () {
+    question.textContent = "do you want the funk?";
+    answerA.textContent = "yeah";
+    answerB.textContent = "funky";
+    answerC.textContent = " funky funk";
+    answerD.textContent = "we want the funk!";
+}
+console.log(nu3)
+
+
+
+let correctAnswers = ["a","b","c"]
+let userAnswers = ["a","b","c"]
+console.log(userAnswers + correctAnswers)
+
+let funkArray = [nu1,nu2,nu3];
+
+console.log(funkArray + " " + "woah it wokred")
+
+
