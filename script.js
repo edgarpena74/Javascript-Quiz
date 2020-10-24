b9c66b266948c2dd7b86914747923e4562c1231c
Edgar Pena <edgarignaciopena@gmail,com>
2020-10-24 16:39:52 -0700

    put timer in scope of answerClick function

--- script.js
+++ script.js
@@ -64,7 +64,32 @@ let answerClick = function(e){
     answersArray.push(value)
     console.log(answersArray + "***** new value")
 
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+            setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
     
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+        
+    }
+    
+    window.onload = function () {
+        var twoMinutes = 60 * 2,
+            display = document.querySelector('#time');
+        startTimer(twoMinutes, display);
+    };
+
+    
+    startTimer();
    
       
    
@@ -78,29 +103,29 @@ let answerClick = function(e){
 }
 
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-        setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//         setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+//         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
     
-}
+// }
 
-window.onload = function () {
-    var twoMinutes = 60 * 2,
-        display = document.querySelector('#time');
-    startTimer(twoMinutes, display);
-};
+// window.onload = function () {
+//     var twoMinutes = 60 * 2,
+//         display = document.querySelector('#time');
+//     startTimer(twoMinutes, display);
+// };
 
 
 
@@ -147,7 +172,8 @@ let nextButton = function click() {
 }
 
 nextQuestion();
-startTimer();
+
+
 
 
 
